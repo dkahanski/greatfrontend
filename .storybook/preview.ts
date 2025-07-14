cc57619eb353a0be748c9d010a61d171f1c95112
dkahanski <dkahanski.lern@gmail.com>
2025-07-14 20:19:20 +0300

    Updates

---
+++ .storybook/preview.ts
@@ -0,0 +1,37 @@
+import { withThemeByClassName } from "@storybook/addon-themes";
+import type { Preview } from "@storybook/react-vite";
+
+/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
+import "../src/styles/tailwind.css";
+
+const preview: Preview = {
+	parameters: {
+		actions: { argTypesRegex: "^on[A-Z].*" },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+		a11y: {
+			// 'todo' - show a11y violations in the test UI only
+			// 'error' - fail CI on a11y violations
+			// 'off' - skip a11y checks entirely
+			test: "todo",
+		},
+	},
+	tags: ["autodocs"],
+	decorators: [
+		// Adds theme switching support.
+		// NOTE: requires setting "darkMode" to "class" in your tailwind config
+		withThemeByClassName({
+			themes: {
+				light: "light",
+				dark: "dark",
+			},
+			defaultTheme: "light",
+		}),
+	],
+};
+
+export default preview;
